d69be792973832d20c1f20df91103abbaa25f8ab
Jan Macenka <jan.macenka@gmail.com>
2020-11-25 14:32:52 +0100

    Fixed typo in Markdown

--- covid_19/settings/markdown_text.py
+++ covid_19/settings/markdown_text.py
@@ -7,7 +7,7 @@ JHCCU_INFO_TEXT = """
 The [data](https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv) is pulled from Johns Hopkins University's [GitHub-Repositors](https://github.com/CSSEGISandData/COVID-19) and visualized using [Dash](https://dash.plotly.com/) which is a Python Wrapper/Framework for [Flask](https://flask.palletsprojects.com/), [Plotly.js](https://plotly.com/) and [React.js](https://reactjs.org/)
 This is an extension to my original [Jupyter-Notebook](https://github.com/jmacenka/jupyternotebook_covid_19_visualization/blob/master/analysis_of_the_covid_19_course.ipynb) based analysis. The [source code for this WebApp](https://github.com/jmacenka/dash-covid-19-visualization) can also be found on Github.
 
-Primarily I built this app for myself but feel free to use. Its not the most beautiful but for 800 lies of code its quite ok I think ;-)
+Primarily I built this app for myself but feel free to use. Its not the most beautiful but for 800 lines of code its quite ok I think ;-)
 
 Have fun exploring the data with views you control.
 
